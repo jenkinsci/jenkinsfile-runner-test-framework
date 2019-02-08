0b8d62d1759e3960bec3b67abd4a5355a82e46ff
Evaristo Gutiérrez <egutierrez@cloudbees.com>
2019-02-08 10:48:59 +0100

    [JENKINS-54391] Hook to verify execution logs (#10)
    * [JENKINS-54391] Store logs into disk when running Jenkinsfile.
    * [JENKINS-54391] Hook for the logs.
    * [JENKINS-54391] Use new hooks in the tests.

--- tests/tests.sh
+++ tests/tests.sh
@@ -18,52 +18,52 @@ oneTimeSetUp() {
 
 test_with_tag() {
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "$downloaded_cwp_jar" "$version" "$current_directory/test_resources/test_with_tag/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
-  execution_should_success "$?" "$jfr_tag" "$jenkinsfile_runner_tag"
-  
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_with_tag/Jenkinsfile")
-  jenkinsfile_execution_should_succed "$?" "$result"
+  execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
+
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_with_tag/Jenkinsfile"
+  jenkinsfile_execution_should_succed "$?"
 }
 
 test_java_opts() {
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "$downloaded_cwp_jar" "$version" "$current_directory/test_resources/test_with_tag/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
-  execution_should_success "$?" "$jfr_tag" "$jenkinsfile_runner_tag"
+  execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
 
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_with_tag/Jenkinsfile")
-  jenkinsfile_execution_should_succed "$?" "$result"
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_with_tag/Jenkinsfile"
+  jenkinsfile_execution_should_succed "$?"
 
   export JAVA_OPTS="-Xmx1M -Xms100G"
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_with_tag/Jenkinsfile")
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_with_tag/Jenkinsfile"
   assertEquals "Should retrieve exit code 0" "0" "$?"
-  message_not_contains "$result" "[Pipeline] End of Pipeline"
-  message_not_contains "$result" "Finished: SUCCESS"
-  message_contains "$result" "Initial heap size set to a larger value than the maximum heap size"
+  logs_not_contains "[Pipeline] End of Pipeline"
+  logs_not_contains "Finished: SUCCESS"
+  logs_contains "Initial heap size set to a larger value than the maximum heap size"
   unset JAVA_OPTS
 }
 
 test_with_default_tag() {
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "$downloaded_cwp_jar" "$version" "$current_directory/test_resources/test_with_tag/packager-config.yml" | grep 'Successfully tagged')
-  execution_should_success "$?" "$jfr_tag" "test_with_default_tag"
+  execution_should_success "$?" "test_with_default_tag" "$jfr_tag"
 }
 
 test_download_cwp_version() {
   default_cwp_jar=$(download_cwp "$working_directory")
-  execution_should_success "$?" "$default_cwp_jar" "cwp-cli-$DEFAULT_CWP_VERSION.jar"
+  execution_should_success "$?" "cwp-cli-$DEFAULT_CWP_VERSION.jar" "$default_cwp_jar"
 
   another_cwp_jar=$(download_cwp "$working_directory" "1.3")
-  execution_should_success "$?" "$another_cwp_jar" "cwp-cli-1.3.jar"
+  execution_should_success "$?" "cwp-cli-1.3.jar" "$another_cwp_jar"
 }
 
 test_with_tag_using_cwp_docker_image() {
   jfr_tag=$(generate_docker_image_from_cwp_docker_image "$current_directory/test_resources/test_with_tag_using_cwp_docker_image/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
-  execution_should_success "$?" "$jfr_tag" "$jenkinsfile_runner_tag"
-  
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_with_tag_using_cwp_docker_image/Jenkinsfile")
-  jenkinsfile_execution_should_succed "$?" "$result"
+  execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
+
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_with_tag_using_cwp_docker_image/Jenkinsfile"
+  jenkinsfile_execution_should_succed "$?"
 }
 
 test_with_default_tag_using_cwp_docker_image() {
   jfr_tag=$(generate_docker_image_from_cwp_docker_image "$current_directory/test_resources/test_with_default_tag_using_cwp_docker_image/packager-config.yml" | grep 'Successfully tagged')
-  execution_should_success "$?" "$jfr_tag" "test_with_default_tag_using_cwp_docker_image"
+  execution_should_success "$?" "test_with_default_tag_using_cwp_docker_image" "$jfr_tag"
 }
 
 test_failing_docker_image() {
@@ -73,18 +73,18 @@ test_failing_docker_image() {
 
 test_jenkinsfile_fail() {
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "$downloaded_cwp_jar" "$version" "$current_directory/test_resources/test_with_tag/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
-  execution_should_success "$?" "$jfr_tag" "$jenkinsfile_runner_tag"
+  execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
 
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_jenkinsfile_fail/Jenkinsfile")
-  jenkinsfile_execution_should_fail "$?" "$result"
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_jenkinsfile_fail/Jenkinsfile"
+  jenkinsfile_execution_should_fail "$?"
 }
 
 test_jenkinsfile_unstable() {
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "$downloaded_cwp_jar" "$version" "$current_directory/test_resources/test_with_tag/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
-  execution_should_success "$?" "$jfr_tag" "$jenkinsfile_runner_tag"
+  execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
 
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_jenkinsfile_unstable/Jenkinsfile")
-  jenkinsfile_execution_should_be_unstable "$?" "$result"
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_jenkinsfile_unstable/Jenkinsfile"
+  jenkinsfile_execution_should_be_unstable "$?"
 }
 
 init_framework
